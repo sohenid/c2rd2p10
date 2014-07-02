7f15fd83b5dc10a635f560f6b1a33b9cedbd9e92
Dheison Silva <dheison@mixd.com.br>
2014-07-02 14:01:25 -0300

    ...

---
+++ cardapio/www/js/ui-menu.js
@@ -0,0 +1,8 @@
+document.write('<div id="menu">');
+document.write('<ul>');
+document.write('<li><a href="categorias.html"><span class="glyphicon glyphicon-th-large"></span> Cardápio</a></li>');
+document.write('<li><a href="update.html"><span class="glyphicon glyphicon-repeat"></span> Atualizar</a></li>');
+document.write('<li><a href="config-login.html"><span class="glyphicon glyphicon-cog"></span> Configurar</a></li>');
+document.write('<li><a href="#" onclick="exit()"><span class="glyphicon glyphicon-off"></span> Fechar</a></li>');
+document.write('</ul>');
+document.write('</div>');
